845a94892cf8c3453890ce9b14b1c0e64d851363
beheos <oscar.sis08@gmail.com>
2023-08-27 12:22:27 -0600

    se crean los getter and setters

--- src/main/java/com/beheos/libreriaApi/PruebaGit.java
+++ src/main/java/com/beheos/libreriaApi/PruebaGit.java
@@ -2,6 +2,21 @@ package com.beheos.libreriaApi;
 
 public class PruebaGit {
 
-	
+	private String nombre;
 	private String paterno;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPaterno() {
+		return paterno;
+	}
+	public void setPaterno(String paterno) {
+		this.paterno = paterno;
+	}
+	
+	
 }
