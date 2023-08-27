5f2e4403965f6c0229c3280f011af1a51ad40288
beheos <oscar.sis08@gmail.com>
2023-08-27 12:24:01 -0600

    se elimina los datos no necesarios

--- src/main/java/com/beheos/libreriaApi/PruebaGit.java
+++ src/main/java/com/beheos/libreriaApi/PruebaGit.java
@@ -3,19 +3,13 @@ package com.beheos.libreriaApi;
 public class PruebaGit {
 
 	private String nombre;
-	private String paterno;
+	
 	
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-	public String getPaterno() {
-		return paterno;
-	}
-	public void setPaterno(String paterno) {
-		this.paterno = paterno;
 	}
 	
 	
